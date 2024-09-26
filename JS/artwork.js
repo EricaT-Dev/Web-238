187bab5800dd223686cad397365499921316372a
EricaT-Dev <ericatalley05@gmail.com>
2024-09-26 18:31:25 -0500

    Create artwork.js

---
+++ JS/artwork.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    //descriptions on hover for rest of images
+    $(".art-pieces:nth-child(4)").hover(function(){
+        $("#desc4").toggle();
+    })
+    $(".art-pieces:nth-child(5)").hover(function(){
+        $("#desc5").toggle();
+    })
+    $(".art-pieces:nth-child(6)").hover(function(){
+        $("#desc6").toggle();
+    })
+    $(".art-pieces:nth-child(7)").hover(function(){
+        $("#desc7").toggle();
+    })
+    $(".art-pieces:nth-child(8)").hover(function(){
+        $("#desc8").toggle();
+    })
+});
