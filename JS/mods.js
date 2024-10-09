3942ae1c254650233772c7e57320673327543a0f
EricaT-Dev <ericatalley05@gmail.com>
2024-10-09 16:47:15 -0500

    Update mods.js

--- JS/mods.js
+++ JS/mods.js
@@ -13,6 +13,9 @@ $(document).ready(function(){
     $(".art-pieces:nth-child(3)").hover(function(){
         $("#desc3").toggle();
     })
+    $(".art-pieces:nth-child(4)").hover(function(){
+        $("#desc4").toggle();
+    })
     //drop down menu for mobile
     $("#dropdown-button").click(function(){
         $(".navigation").toggle();
@@ -66,10 +69,90 @@ $(document).ready(function(){
     //top menu scrolls with reader
     $(window).scroll(function(){ 
         $('#top-header').css('top', $(document).scrollTop()); 
-      }); 
+      }) 
     //animate scroll to top when click link
     $("#top-link").click(function(){
         $("html").animate({scrollTop: 0}, "slow");
         return false;
     })
+    //lightbox function for images
+    //adds dark background on click and close button
+    $("a.imgLightbox").click(function(){
+        $("#overlay").css("display", "block")
+        $("#closeOverlay").css("display", "block")
+    })
+    //removes background, image, and close button on click
+    $("#closeOverlay").click(function(){
+        $("#overlay").css("display", "none")
+        $("#closeOverlay").css("display", "none")
+        $(".lightBox-imgs").css("display", "none")
+        $("#art-10").removeClass("darken")
+        $("#art-10").addClass("show")
+    })
+    //adds images for each clicked
+    $("#imageOneLink").click(function(){
+        $("#lightBox-img1").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageTwoLink").click(function(){
+        $("#lightBox-img2").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageThreeLink").click(function(){
+        $("#lightBox-img3").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageFourLink").click(function(){
+        $("#lightBox-img4").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageFiveLink").click(function(){
+        $("#lightBox-img5").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageSixLink").click(function(){
+        $("#lightBox-img6").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageSevenLink").click(function(){
+        $("#lightBox-img7").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageEightLink").click(function(){
+        $("#lightBox-img8").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageNineLink").click(function(){
+        $("#lightBox-img9").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    $("#imageTenLink").click(function(){
+        $("#lightBox-img10").css("display", "block")
+        $("#art-10").addClass("darken")
+    })
+    //resize clickable images on hover
+    $(".art-pieces").hover(function(){
+        $(this).animate(
+            {width:"+=2%"}
+        )
+    },
+    function(){
+        $(this).animate(
+            {width:"-=2%"}
+        )
+    })
+    //slideshow for Vangoh Lamp
+    $(document).ready(function(){
+        slideShow();
+      });
+      function slideShow() {
+        var current = $('#slideshow .showing');
+        var next = current.next().length ? current.next() : 
+        current.siblings().first();
+      
+        current.hide().removeClass('showing');
+        next.fadeIn().addClass('showing');
+        
+        setTimeout(slideShow, 2500);
+      }
 });
